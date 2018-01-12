956cd44ad5dbd7c0e7713a715c979bc6a0455ee6
Anderson Valerio <anderson@domain.fulltime>
2018-01-12 14:40:07 -0200

    Certo

---
+++ application/views/template/menu.php
@@ -0,0 +1,86 @@
+<div class="navbar-default sidebar" role="navigation">
+                    <div class="sidebar-nav navbar-collapse">
+                        <ul class="nav" id="side-menu">
+                            <li class="sidebar-search">
+                                <div class="input-group custom-search-form">
+                                    <input type="text" class="form-control" placeholder="Search...">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-primary" type="button">
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                                </span>
+                                </div>
+                                <!-- /input-group -->
+                            </li>
+                            <li>
+                                <a href="index.html" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="index.html" class="active"><img src="<?php echo base_url()?>img/icones/009-technology.png"> Clientes</a>
+                            </li>
+                            <li>
+                                <a href="index.html" class="active"><img src="<?php echo base_url()?>img/icones/008-customer-service.png"> Produtos</a>
+                            </li>
+                            <li>
+                                <a href="index.html" class="active"><img src="<?php echo base_url()?>img/icones/007-cart.png"> Serviços</a>
+                            </li>
+                            <li>
+                                <a href="index.html" class="active"><img src="<?php echo base_url()?>img/icones/006-choices.png"> Ordens de serviço</a>
+                            </li>
+                            <li>
+                                <a href="index.html" class="active"><img src="<?php echo base_url()?>img/icones/005-money-bag.png"> Vendas</a>
+                            </li>
+                            <li>
+                                <a href="index.html" class="active"><img src="<?php echo base_url()?>img/icones/004-office-material.png"> Arquivos</a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url()?>img/icones/003-graph.png"> Financeiro<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <li>
+                                        <a href="flot.html">Lançamentos</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url()?>img/icones/002-bars.png"> Relatórios<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <li>
+                                        <a href="flot.html">Clientes</a>
+                                    </li>
+                                    <li>
+                                        <a href="flot.html">Produtos</a>
+                                    </li>
+                                     <li>
+                                        <a href="flot.html">Serviços</a>
+                                    </li>
+                                    <li>
+                                        <a href="flot.html">Ordens de serviço</a>
+                                    </li>
+                                    <li>
+                                        <a href="flot.html">Financeiro</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+                            </li>
+                            <li>
+                                <a href="#"><img src="<?php echo base_url()?>img/icones/001-settings.png"> Configurações<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <li>
+                                        <a href="flot.html">Usuários</a>
+                                    </li>
+                                     <li>
+                                        <a href="flot.html">Emitente</a>
+                                    </li>
+                                    <li>
+                                        <a href="flot.html">Permissões</a>
+                                    </li>
+                                    <li>
+                                        <a href="flot.html">Backup</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+                            </li>
+                        </ul>
+                    </div>
+                </div>
